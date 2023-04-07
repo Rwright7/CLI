2e01b096706f72fbd739c78921936e8a94d0e789
roshawn <89256902+Rwright7@users.noreply.github.com>
2023-04-07 08:01:08 -0500

    Update splitfunction.h

--- splitfunction.h
+++ splitfunction.h
@@ -1,6 +1,7 @@
 #ifndef  SPLIT_FUNCTION_H
 #define  SPLIT_FUNCTION_H
 
-void splitfunction(cstr *input);
+#include <stc/forward.h>
+void splitfunction(cstr *input, csview *left, csview *right);
 
 #endif
